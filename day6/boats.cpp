6f8538a6b00dec7e7350d8cfe37ad1abe29b3581
Skyblueborb <tomaszborbely0710@gmail.com>
2023-12-08 18:58:13 +0100

    AoC: Day 6: Code improvements
    shit now doesn't have a 5 trillion loop in a 5 trillion loop in part 2
    used my brain a little and its way faster.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- day6/boats.cpp
+++ day6/boats.cpp
@@ -22,12 +22,6 @@ std::vector<std::string> split (const std::string &s, char delim);
 
 struct boost::cnv::by_default : boost::cnv::lexical_cast {};
 
-/*
-*   Hello there savvy adventurer looking upon my shitass CPP code.
-*   I shall warn thou, as this shit needs these compiler flags: -Ofast -ffast-math
-*   for example: clang++ -Ofast -ffast-math boats.cpp -o boats
-*   In order for part 2 to finish in a sane amount of time.
-*/
 int main (void) {
     std::string time;
     std::string distance;
@@ -54,13 +48,11 @@ int solve_part1(std::string time, std::string distance) {
     std::vector<int> distanceRecords;
     for (auto i : v) timeAllowed.push_back(boost::convert<int>(i).value());
     for (auto i : v2) distanceRecords.push_back(boost::convert<int>(i).value());
-    int distancePerMs = 0, finalDistance = 0, permutations = 0, final_score = 0;
+    int finalDistance = 0, permutations = 0, final_score = 0;
     for (int k = 0; k < timeAllowed.size(); k++) {
+        // i - distancePerMs
         for (int i = 0; i < timeAllowed.at(k); i++) {
-            distancePerMs = i;
-            for (int j = 0; j < timeAllowed.at(k) - i; j++) {
-                finalDistance+=distancePerMs;
-            }
+            finalDistance=i*(timeAllowed.at(k) - i);
             if(finalDistance > distanceRecords.at(k)) permutations++;
             finalDistance = 0;
         }
@@ -91,14 +83,11 @@ int solve_part2(std::string time, std::string distance) {
     buffer = "";
     for (auto i : v2) buffer += i;
     distanceRecord = boost::convert<u_int64_t>(buffer).value();
-    u_int64_t distancePerMs = 0, finalDistance = 0, permutations = 0;
-    for (u_int64_t i = 0; i < timeAllowed; i++) {
-        distancePerMs = i;
-        for (u_int64_t j = 0; j < timeAllowed - i; j++) {
-            finalDistance+=distancePerMs;
-        }
+    u_int64_t finalDistance = 0, permutations = 0;
+    // i - distancePerMs
+    for (u_int64_t i = 0; i < timeAllowed; i++, finalDistance = 0) {
+        finalDistance=i*(timeAllowed-i);
         if(finalDistance > distanceRecord) permutations++;
-        finalDistance = 0;
     }
     return permutations;
 }
